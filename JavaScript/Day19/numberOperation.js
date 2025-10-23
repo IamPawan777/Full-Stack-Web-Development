8655ce84c90084f6bafd3a17f9589d5c62dedca2
IamPawan <iampawanbisht7@gmail.com>
2025-10-23 23:59:33 +0530

    Day19

---
+++ JavaScript/Day19/numberOperation.js
@@ -0,0 +1,41 @@
+// // toFixed()....
+// let a = 6789.7890;
+// console.log(a.toFixed(2));           // O/P: 6789.79  -> print as a string...
+// console.log(Number(a.toFixed(2)));            
+
+
+
+
+// // toPrecision()..........
+// let a = 12.34567;
+// console.log(a.toPrecision(3));       // 12.3
+
+
+
+
+// // toString()..........
+// let a = 12.34567;
+// console.log(a.toString());
+
+
+
+// // primitive data as object.....
+// let a = new Number(40);
+// let b = new Number(40);
+// console.log(a, b);          // [Number: 40] [Number: 40]
+
+
+
+
+// Math.abs()/ .ceil() / .floor() / .log() / .random().....
+console.log(Math.abs(-23));
+console.log(Math.ceil(34.34));
+console.log(Math.floor(34.34));
+console.log(Math.log10(10));
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10));          // 0 to 9
+console.log(Math.floor(Math.random()*10)+1);          // 1 to 10
+console.log(Math.floor(Math.random()*9999-1000+1)+1000);     // 4 digit no
+
+console.log(Number.MIN_VALUE);
+
